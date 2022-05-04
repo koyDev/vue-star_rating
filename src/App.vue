d6b96daf90fc700628c98d476e1b5317ad4b5010
koyDev <likoyao@gmail.com>
2022-05-04 16:26:51 +0800

    bug fixed

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="star">
+    {{ hoverCnt }}
+    <span
+      v-for="(i, idx) in starCnt"
+      :key="idx"
+      class="px-1"
+      :class="{'active': hoverCnt >= idx}"
+      @click="selectHandle(idx)"
+      @mouseover="hoverHandle(idx)"
+      @mouseleave="leaveHandle"
+    >
+      <i class="fa-solid fa-star" />
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      starCnt: 10,
+      selectedCnt: 0,
+      hoverCnt: 0
+    }
+  },
+  methods: {
+    selectHandle (idx) {
+      this.selectedCnt = idx
+      this.$emit('selected', this.selectedCnt)
+    },
+    hoverHandle (idx) {
+      this.hoverCnt = idx
+    },
+    leaveHandle () {
+      this.hoverCnt = this.selectedCnt
+    }
+  }
+}
+</script>
+
+<style>
+  .star {
+    color: #ddd;
+  }
+  .star .active i{
+    color: #ebd72d;
+  }
+</style>
